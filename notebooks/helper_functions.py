be025c4ed8303c3d65db48f0636e14989a82105c
George Perdrizet <george@perdrizet.org>
2025-08-05 17:47:01 +0000

    Refactored split model class

--- notebooks/helper_functions.py
+++ notebooks/helper_functions.py
@@ -2,6 +2,8 @@
 
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import root_mean_squared_error, r2_score
 
 
 def get_level_counts(data_df: pd.DataFrame, features: list) -> pd.DataFrame:
@@ -30,3 +32,44 @@ def encode_features(data_df: pd.DataFrame, encoder: OneHotEncoder, nominal_featu
     )
 
     return pd.concat([data_df.drop(columns=nominal_features), encoded_df], axis=1)
+
+
+class SplitModel:
+    def __init__(self, training_df, testing_df):
+        self.smoker_training_df = training_df[training_df['smoker_yes'] == 1].copy()
+        self.nonsmoker_training_df = training_df[training_df['smoker_yes'] == 0].copy()
+        self.smoker_training_df.drop('smoker_yes', axis=1, inplace=True)
+        self.nonsmoker_training_df.drop('smoker_yes', axis=1, inplace=True)
+
+        self.smoker_testing_df = testing_df[testing_df['smoker_yes'] == 1].copy()
+        self.nonsmoker_testing_df = testing_df[testing_df['smoker_yes'] == 0].copy()
+        self.smoker_testing_df.drop('smoker_yes', axis=1, inplace=True)
+        self.nonsmoker_testing_df.drop('smoker_yes', axis=1, inplace=True)
+        
+        self.smoker_model = LinearRegression()
+        self.nonsmoker_model = LinearRegression()
+        self.smoker_predictions = None
+        self.nonsmoker_predictions = None
+
+    def fit(self):
+        # Fit the model on the training data
+
+        self.smoker_model.fit(self.smoker_training_df.drop('charges', axis=1), self.smoker_training_df['charges'])
+        self.nonsmoker_model.fit(self.nonsmoker_training_df.drop('charges', axis=1), self.nonsmoker_training_df['charges'])
+
+    def predict(self):
+        # Make predictions on the testing data
+
+        self.smoker_predictions = self.smoker_model.predict(self.smoker_testing_df.drop('charges', axis=1))
+        self.nonsmoker_predictions = self.nonsmoker_model.predict(self.nonsmoker_testing_df.drop('charges', axis=1))
+
+    def evaluate(self):
+        # Evaluate the model performance
+
+        predictions = self.smoker_predictions.tolist() + self.nonsmoker_predictions.tolist()
+        labels = self.smoker_testing_df['charges'].tolist() + self.nonsmoker_testing_df['charges'].tolist()
+
+        rmse = root_mean_squared_error(labels, predictions)
+        rsq = r2_score(labels, predictions)
+        
+        return rmse, rsq
